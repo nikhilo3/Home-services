bd0205529985bad6f790ec88183c3421e4001703
nikhilo3 <nikhilladani03@gmail.com>
2024-02-17 08:52:53 +0530

    all changes

--- assest/js/servicedetail.js
+++ assest/js/servicedetail.js
@@ -33,38 +33,42 @@ let subcontain = document.querySelector('[subcontain]');
                 <h2>${name}</h2>
                 <div class="scroll-container">
                     <div class="subitem">
-                    <div class="d-flex" style="gap:50px;" subwrapper></div>
+                    <div class="abc" subwrapper></div>
                     </div>
                 </div>
                 `
 
             sub_service.forEach(({ id, title, rating, price, image, description1, description2, description3 }) => {
                 let newsubcard = document.createElement('div');
-                newsubcard.classList.add("row", "p-lg-3", "col-lg-4", "pb-0", "pe-lg-0", "pt-lg-2", "align-items-center", "rounded-5", "shadow-lg", "justify-between")
-                newsubcard.style.borderRadius = '10px';
+                newsubcard.classList.add("cards");
                 newsubcard.innerHTML = `
                     
-        <div class="col-lg-8 p-lg-3">
-          <h2 class=" fw-bold lh-1" style="font-size: 24px;">${title}</h2>
-          <div class="d-flex " style="align-items:center;">
-          <img width="42" height="42"
+                <div class="carddetailes">
+                <div class="border-hero">
+                    <h2 class="title">${title}</h2>
+                    <div class="reting" style="display:flex; align-items: center;">
+                        <img width="32" height="32" 
                             src="https://img.icons8.com/sf-black-filled/64/000000/rating-circled.png"
                             alt="rating-circled" />
                         <span>${rating}</span>
+
+                    </div>
+                </div>
+                <div class="description">
+                    <ul style="padding-left: 15px">
+                        <li>${description1}</li>
+                        <li>${description2}</li>
+                    </ul>
+                </div>
+                <div class="price">
+                    ${price}
+                </div>
             </div>
-          <ul style=" font-size:12px; padding-left: 20px; line-height: 20px; text-align: justify; ">
-            <li style="list-style:disc;">${description1}</li>
-            <li style="list-style:disc;">${description2}</li>
-            <li style="list-style:disc;">${description3}</li>
-          </ul>
-          <div style="padding-left: 20px;">
-          <span class="fw-bold" style="font-size:22px;">${price}</span>
-        </div>
-        </div>
-        <div class="col-lg-4 p-0 shadow-lg" style=" position: relative; text-align: center;">
-          <img class="rounded" src="${image}" alt="" width="164px" height="170px" style="object-fit: cover;">
-            <button type="button" class="btn btn-primary btn-lg px-4 me-md-2 fw-bold btncart addcart" style="position: absolute; bottom: -24px; right: 25px;">ADD</button>
-        </div>
+            <div class="card-image">
+                <img src=${image}>
+                <button class="add-button btn btncart addcart">ADD</button>
+            </div>
+            </div>
                     `
                 newservices.querySelector('[subwrapper]').appendChild(newsubcard);
 
@@ -110,24 +114,7 @@ async function addtocart(id, title, price, image) {
         else {
             console.log('failed to add item to cart', response.statusText);
         }
-        // const existingItem = cartItems.find((item) => {
-        //     item.id === id;
-        // });
-
-        // if (existingItem) {
-        //     existingItem.quantity += 1;
-        // }
-        // else {
-        //     cartItems.push({
-        //         id: id,
-        //         title: title,
-        //         price: price,
-        //         image: image,
-        //         quantity: 1
-        //     });
-        // }
-
-        // localStorage.setItem('cartItems', JSON.stringify(cartItems));
+        
     }
     catch (error) {
         console.log('Error adding item to cart:', error);
